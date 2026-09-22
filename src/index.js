71483eb8dcaee284d4ef1b93fd8e8dab2d2d6f70
Omkar Raskar <omkarraskar9192@gmail.com>
2026-09-22 17:20:06 +0530

    new packeges add cors/cookies-parser

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import dotenv from 'dotenv'
 import express from 'express';
 import ConnetDB from './db/index.js'
 dotenv.config();
+const app = express();
 
 
 ConnetDB()
+.then(  
+    console.log("Connetion sucessful"),
+    process.exit(0)
+)
+.catch(
+    app.listen("error",(error)=>{
+        console.log(`Error in DB Connection : ${error}` )
+    }).
+    process.exit(1)
+)
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+
